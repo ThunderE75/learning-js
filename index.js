1f47a6efacdada7250ff47a62dd2f37f3fc9e766
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-15 12:05:33 +0530

    Variables

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-document.getElementById("myHeading").textContent = `JS-Hello`;
-document.getElementById("myParagraph").textContent = `i love pizza!`;
+let age = 25;
+let ageString = "25";
+let name = 'navneet';
+let online = true
+
+console.log(typeof age)
+console.log(typeof ageString)
+console.log(typeof name)
+console.log(typeof online)
+
+console.log(age);
+console.log(`Your name is ${name}`);
+console.log(`Your age is ${age}`);
+console.log(`Are you online: ${online}`);
