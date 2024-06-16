9b1b3f123030a84240140bcababf667138ef357c
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-16 10:50:28 +0530

    If & nested if else statement

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const MIN = 50;
-const MAX = 100;
-let ans;
-// ans = Math.trunc(Math.random()*100);
+let btn = document.getElementById("btn");
+let display = document.getElementById("display");
 
-let btn = document.getElementById("rand");
-let dis = document.getElementById("display");
+btn.onclick = function () {
+    let age = Number(document.getElementById("input").value);
 
-btn.onclick = function(){
-  ans = Math.floor(Math.random() * (MAX - MIN) ) + MIN;
-  dis.textContent = ans;
-}
-
-
+    if (age >= 60) {
+        display.textContent = "You are too old for this ride";
+    } else if (age == 0) {
+        display.textContent = "You were just born, you can't ride";
+    } else if (age < 0) {
+        display.textContent = "Please enter correct age!";
+    } else if (age <= 12) {
+        display.textContent = "You are too young, you can't ride";
+    } else display.textContent = "You go on this ride";
+};
