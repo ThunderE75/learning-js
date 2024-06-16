54fc4adc1d412b245bcdcf0138efc86ce15be953
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-16 11:32:03 +0530

    Switch Case

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-/* 
-Ternary operator:	 	Shortcut of if & else
-										Condition ? codeIfTrue : codeIfFalse
-*/
-
-let age = 26;
+let day = "pizza";
+let message;
 let display = document.getElementById("display");
 
-let message = age > 18 ? "You are an adult" : "You are a Minor";
-
+switch (day) {
+    case 1:
+        message = "It's Sunday!";
+        break;
+    case 2:
+        message = "It's Monday";
+        break;
+    case 3:
+        message = "It's Tuesday";
+        break;
+    case 4:
+        message = "It's Wednesday";
+        break;
+    case 5:
+        message = "It's Thursday";
+        break;
+    case 6:
+        message = "It's Friday";
+        break;
+    case 7:
+        message = "It's Saturday";
+        break;
+    default:
+        message = `${day} is not a valid day.`;
+        break;
+}
 display.textContent = message;
