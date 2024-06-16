c620f4a9f81b58a85701d6f0b71cb1071fb97674
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-16 09:48:57 +0530

    Math Class

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-let display = document.getElementById("display");
-let btnDec = document.getElementById("decrement");
-let btnReset = document.getElementById("reset");
-let btnInc = document.getElementById("increment");
+let x = 21.5;
+let y = 2;
+let z = -56.4;
+let ans;
 
-let count = 0;
+// ans = Math.round(x);
+// ans = Math.floor(x);
+// ans = Math.ceil(x);
+// ans = Math.trunc(x);
+// ans = Math.trunc(x);
+// ans = Math.pow(x,y);
+// ans = Math.sqrt(x);
+// ans = Math.log(x);
+// ans = Math.sin(x);
+// ans = Math.cos(x);
+// ans = Math.tam(x);
+// ans = Math.abs(z);
+// ans = Math.sign(z);
+// ans = Math.max(x,y,z);
+ans = Math.min(x,y,z);
 
-btnDec.onclick = function () {
-  count--;
-  display.textContent = count;
-};
-
-btnReset.onclick = function () {
-  display.textContent = 0;
-};
-
-btnInc.onclick = function () {
-  count++;
-  display.textContent = count;
-};
+document.getElementById("dis").textContent = ans;
