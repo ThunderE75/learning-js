93aac230785c49fb44675657adbf6989f5ad91a9
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-17 18:54:29 +0530

    String Slicing

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-let s1 = "Thunderized";
-let display = document.getElementById("display");
+let s1 = "Thunder Emperor";
+// let display = document.getElementById("display");
+let dis1 = document.getElementById("dis1");
+let dis2 = document.getElementById("dis2");
 
+// Hard Coded indices
+// dis1.textContent = s1.slice(0,7);
+// dis1.textContent = s1.slice(8,15);
+// dis1.textContent = s1.slice(8);
+// dis1.textContent = s1.slice(-7);
 
-// display.textContent = s1.charAt(0);
-// display.textContent = s1.indexOf('e');
-// display.textContent = s1.lastIndexOf('e');
-// display.textContent = s1.length;
+// Dynamic Indices
+dis1.textContent = s1.slice(0,s1.indexOf(' '));
+dis2.textContent = s1.slice(s1.indexOf(' ')+1);
 
-let s2 = "   Thunder Emperor   ";
-// display.textContent = s2.trim();
-// display.textContent = s2.toUpperCase();
-// display.textContent = s2.toLowerCase();
-// display.textContent = s2.repeat(3);
-
-let s3 = "Thunder Emperor";
-// display.textContent = s2.startsWith('T');       // Returns Boolean
-// display.textContent = s2.endsWith('T');       // Returns Boolean
-// display.textContent = s2.includes('Emperor');       // Returns Boolean
-
-let pNum = '123-456-7890';
-// display.textContent = pNum.replace('-','');
-// display.textContent = pNum.replaceAll('-','');
-// display.textContent = pNum.padStart(15,'x');
-display.textContent = pNum.padEnd(15,'x');
