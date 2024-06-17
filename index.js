00a2c16d757b685316e3977bb25044359acea31b
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-17 19:59:43 +0530

    Number Guessing Game

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-let s1 = "Thunder Emperor";
-let display = document.getElementById("display");
-// let dis1 = document.getElementById("dis1");
-// let dis2 = document.getElementById("dis2");
+const MIN = 1;
+const MAX = 100;
+let rand = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
 
-let input = window.prompt("Enter a Name");
-input = input.trim().charAt(0).toUpperCase() + input.trim().slice(1).toLowerCase();
+let attempts = 0;
+let guess;
+let running = true;
 
-display.textContent = input; 
+while (running) {
+    // window.alert(`DEBUGGING: Random Number = ${rand}`)
+    guess = window.prompt(`Attempt: ${attempts}   Enter a guess between ${MIN} - ${MAX}`);
+    guess = Number(guess);
+    if (isNaN(guess)) {
+        window.alert(`${guess} is mot a number, please enter a valid number.`);
+    } else if (guess < MIN || guess > MAX) {
+        window.alert(`Your guess is out of bounds!`);
+    } else {
+        attempts++;
+        if (guess > rand) {
+            window.alert(`Your guess too high!`);
+        } else if (guess < rand) {
+            window.alert(`Your guess too low!`);
+        } else if (guess == rand) {
+            window.alert(
+                `You guessed the correct number (${rand}) in ${attempts} attempts!`
+            );
+            running = false;
+        }
+    }
+}
