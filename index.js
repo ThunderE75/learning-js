b7d72a97c11621a603c9bea0bc831dbb7d9b08a9
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-17 19:07:38 +0530

    Method Chaining

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 let s1 = "Thunder Emperor";
-// let display = document.getElementById("display");
-let dis1 = document.getElementById("dis1");
-let dis2 = document.getElementById("dis2");
+let display = document.getElementById("display");
+// let dis1 = document.getElementById("dis1");
+// let dis2 = document.getElementById("dis2");
 
-// Hard Coded indices
-// dis1.textContent = s1.slice(0,7);
-// dis1.textContent = s1.slice(8,15);
-// dis1.textContent = s1.slice(8);
-// dis1.textContent = s1.slice(-7);
+let input = window.prompt("Enter a Name");
+input = input.trim().charAt(0).toUpperCase() + input.trim().slice(1).toLowerCase();
 
-// Dynamic Indices
-dis1.textContent = s1.slice(0,s1.indexOf(' '));
-dis2.textContent = s1.slice(s1.indexOf(' ')+1);
-
+display.textContent = input; 
