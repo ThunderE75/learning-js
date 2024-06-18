c5eafbe3903aab1f1d1d60c7af74a84b3a660746
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-18 06:03:02 +0530

    Array Basics

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-let ctf = document.getElementById("ctf");
-let ftc = document.getElementById("ftc");
-let dis = document.getElementById("dis1");
-let result;
+let dis1 = document.getElementById("dis1");
 
-function convert() {
-    let data = document.getElementById("txtbox").value;
-    if (ctf.checked) {
-        result = (data * 9) / 5 + 32;
-        dis.textContent = result.toFixed(2) + "°F";
-    } else if (ftc.checked) {
-        result = (data - 32) * (5 / 9);
-        dis.textContent = result.toFixed(2) + "°C";
-    } else dis.textContent = "Please select a unit.";
-}
+let arr = ["Apple", "Banana", "Coconut"]
+
+console.log(arr.length);        // Size
+
+arr.push('Pineapple');          // Adds at end
+console.log(arr);
+arr.pop();                      // Removes from end
+console.log(arr);
+
+arr.sort().reverse();
+
+
+arr.unshift('Pineapple');       // Adds from front
+console.log(arr);               
+arr.shift();                    // Removes from front
+console.log(arr);           
+
+arr.sort();
+console.log(arr);           
+
+console.log(arr.indexOf('Banana'));
+
+console.log(...arr);            // Spread Operator (...)
+                                // unpacks an array
+
+
+/* // BASICS
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+
+console.log(arr);
+arr[4] = "Guava";
+
+console.log(arr[3]);
+console.log(arr[4]);
+
+console.log(arr);
+*/
+
+
+
+
+
+// dis1.textContent = "Hello";
