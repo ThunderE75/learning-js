8cda05382b6f2e887f1e03fad513660f12117744
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-19 09:30:51 +0530

    .map method

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-// For each :   Iterate over each element of the array and apply a function
+// map :        Iterate over each element of the array and apply a function
+//              Same as forEach but returns a new array,
+//              so it needs to be stored in a variable
 //              Element, Index, Array are provided, so you can use it as args
 //              function(element, index, array){....}
 
 let arr = [1, 2, 3, 4, 5];
-arr.forEach(triple);
 
-function display(element) {
-    console.log(element);
-}
+let tripled = arr.map(triple);
 
-function double(element) {
-    element *= 2;
-    display(element);
+function triple(element) {
+    return element * 3;
 }
-
-function triple(element, index, array) {
-    // array[index] === element (just a different way to assess it)
-    array[index] = element * 3;
-    display(array[index]);
-}
-// display(array);
+console.log(tripled);
