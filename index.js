f8b18c21352f62d593b4262b13515c0c2305a965
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-19 09:54:05 +0530

    .reduce()

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// filter :     Create a mew array by filtering them by using callback
+// .reduce()    Reduce the elements of am array into a single value
+//              After first iteration, the returned value is passed 
+//              as the first argument in the next iteration 
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let evenEle = arr.filter(isEven);
-console.log(evenEle);
+let result = arr.reduce(sum);
+console.log(result);
 
-function isEven(element) {
-    return element % 2 === 0;
+function sum(accumulator, current) {
+    return accumulator + current;
 }
