34a14045c7f0d2a4f36f69da7f4953a8c7787882
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-19 10:39:41 +0530

    ‼️ Function Expression

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-// .reduce()    Reduce the elements of am array into a single value
-//              After first iteration, the returned value is passed 
-//              as the first argument in the next iteration 
+// Function Expression      A way to define a function as a value or variable
+//                          Used when we only need to use that function few times,
+//                          Prevents polluting the global namespace
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let result = arr.reduce(sum);
-console.log(result);
+// Normal Way 
+// ################################
+let result1 = arr.map(square);
+console.log(result1);
 
-function sum(accumulator, current) {
-    return accumulator + current;
+function square(element) {
+    return Math.pow(element,2);
+}
+
+// The Function Expression way
+// ################################
+let result2 = arr.map(function(element) {
+    return Math.pow(element,2);
+});
+console.log(result2);
+
+
+
+// Normal function
+function square(element) {
+    return Math.pow(element,2);
+}
+
+// Function Expression
+//  - no name needed for function
+//  - instead of saving it as a variable, just pass it as is
+let sqr = function(element) {
+    return Math.pow(element,2);
 }
