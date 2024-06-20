4e29f1be009e952d59c0c0081ddc0e62b4afde6d
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-20 09:26:24 +0530

    Constructors

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-// This         Refers to the current object 
-//              object.property === this.property
-//              Useful because when using a using a property inside 
-//              the object, we don't need to use the identifier of 
-//              the object, which might change. 
+// Constructor      A special function used to make instance of an objects
+//                  Conventionally, the identifier name starts with upper case
+//                  used to define the properties of that object
 
-const person1 = {
-    firstName: "Spongebob",
-    lastName: "Squarepants",
-    age: 30,
-    isEmployed: true,
-    greet:function() {return `Hello, I am ${this.firstName}!`},
-    // Doesn't work with arrow func. 
-    // https://stackoverflow.com/questions/28798330/arrow-functions-and-this
+// Example object
+// const car = {
+//     make: "Ford",
+//     model: "Mustang",
+//     year: 2024,
+//     inProduction: true
+// }
 
+function Car(make, model, year, inProduction) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.inProduction = inProduction;
+    this.drive=function() {return `You are driving a ${this.year} ${this.make} ${this.model}`}
 }
 
-console.log(person1.firstName);
-console.log(person1.lastName);
-console.log(person1.age);
-console.log(person1.isEmployed);
-console.log(person1.greet());
+// Making an instance of a car
+const car1 = new Car("Ford","Mustang",2024,true);
+
+console.log(car1.make);
+console.log(car1.model);
+console.log(car1.year);
+console.log(car1.inProduction);
+console.log(car1.drive());
