e14d4a82738c77fe82b74a8b000a7f90853ec1f3
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-20 09:10:03 +0530

    ‼️This

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-// Objects      A collection of properties & methods 
-//              object = {key:value,
-//                        function(),} 
-//              They can be invoked using dot notation
+// This         Refers to the current object 
+//              object.property === this.property
+//              Useful because when using a using a property inside 
+//              the object, we don't need to use the identifier of 
+//              the object, which might change. 
 
 const person1 = {
     firstName: "Spongebob",
     lastName: "Squarepants",
     age: 30,
     isEmployed: true,
-    greet:()=>{return "Hello, I am Spongebob!"}
-}
+    greet:function() {return `Hello, I am ${this.firstName}!`},
+    // Doesn't work with arrow func. 
+    // https://stackoverflow.com/questions/28798330/arrow-functions-and-this
 
-const person2 = {
-    firstName: "Patrick",
-    lastName: "Star",
-    age: 42,
-    isEmployed: false,
-    greet:()=>{return "Hi, i'm Patrick!"}
 }
 
 console.log(person1.firstName);
@@ -24,9 +20,3 @@ console.log(person1.lastName);
 console.log(person1.age);
 console.log(person1.isEmployed);
 console.log(person1.greet());
-
-console.log(person2.firstName);
-console.log(person2.lastName);
-console.log(person2.age);
-console.log(person2.isEmployed);
-console.log(person2.greet());
