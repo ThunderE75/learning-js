19fd75a6119fba6e975ed8707cf3371f1cf3d609
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-21 08:05:28 +0530

    Destructuring

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-// Setter   A special method that makes a property writeable.
-// Getter   A special method that makes a property Readable.
+//  Destructuring       A way extract value from arrays & objects
+//                      the assign them to variables in a different way
+//                      [] = Array destructuring
+//                      {} = Object destructuring
 
-class Rectangle {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-    }
+// Example 1 : Swap variables
+let a = 5;
+let b = 10;
+[a, b] = [b, a];
+console.log(a, b);
 
-    // _varName = it means that it's an internal variable &
-    // others should not access it directly, but you can
+// Example 2 : Swap elements of array
+let numbers = ["zero", "one", "two", "three", "four", "five"];
+[numbers[1], numbers[3]] = [numbers[3], numbers[1]];
+console.log(numbers);
 
-    set width(inputWidth) {
-        if (inputWidth > 0) {
-            this._width = inputWidth;
-        } else {
-            console.error("Width should be positive.");
-        }
-    }
+// Example 3 : Extract elements of array
+let colors = ["red", "green", "blue", "yellow", "black", "white"];
+const [primaryOne, primaryTwo, primaryThree, ...otherColors] = colors;
+console.log(primaryOne, primaryTwo, primaryThree);
+console.log(otherColors);
 
-    set height(inputHeight) {
-        if (inputHeight > 0) {
-            this._height = inputHeight;
-        } else {
-            console.error("Height should be positive.");
-        }
-    }
-    get width() {
-        return this._width;
-    }
-    get height() {
-        return this._height;
-    }
-    get area() {
-        return this._height * this._width;
-    }
+// Example 4 : Object destructuring
+const person1 = {
+    fName: "Spongebob",
+    lName: "Squarepants",
+    age: 24,
+    job: "Cook",
+};
+const person2 = {
+    fName: "Patrick",
+    lName: "Star",
+    age: 26,
+};
+
+// const { fName, lName, age, job = "Unemployed" } = person1;
+const { fName, lName, age, job = "Unemployed" } = person2;      
+// here 'unemployed is a default value incase the property doesn't exist'
+console.log(fName, lName, age, job);
+
+// Example 5 : Destructuring in function parameter
+function displayPerson({first,last,age,job='Unemployed'}) {
+    console.log(`First Name: ${first}`);
+    console.log(`Last Name: ${last}`);
+    console.log(`Age: ${age}`);
+    console.log(`Job: ${job}`);
 }
-
-const rect1 = new Rectangle(10, 20);
-
-// rect1.height=-5;
-// rect1._height=-5;
-
-console.log(rect1.width);
-console.log(rect1.height);
-console.log(rect1.area);
-
-// const rect2 = new Rectangle(-6900, "Pizza"); // Shouldn't be possible
-// console.log(rect2.width);
-// console.log(rect2.height);
+displayPerson(person1);
+displayPerson(person2);
