56170212d4978dc1d66c05dcf5023a039551f01b
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-21 17:42:23 +0530

    Closure

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
-//  date Objects
-//
+//  Closure     Function inside Function (Nested Function)
+//              The inside function has access to all variables in outer
+//              Used in JS Frameworks : React, Angular, Vue
 
-const date = new Date();
-console.log(date);
+function outer() {
+    let msg = "hello";
+    function inner() {
+        console.log(msg);
+    }
+    inner();
+}
+// inner();     // can't call inner function directly
+outer();
 
-// Date(year, month, day, hour, min, sec, ms)
-// The Months are zero indexed, so 0 = Jan, 11 = Dec
-const date1 = new Date(2024, 6, 21, 16, 56, 24);
-console.log(date1);
+// It's also useful to maintain state
+function createCounter() {
+    let count = 0;
+    function increment() {
+        count += 1;
+        console.log(`Current count : ${count}`);
+    }
+    function getCount() {
+        return count;
+    }
+    return { increment, getCount };
+    // Returning the increment as object
+    // So it can be accessed
+}
 
-const date2 = new Date("2024-06-21T16:57:49Z");
-console.log(date2);
-
-// we can also just use epoch time in seconds
-const date3 = new Date(0);
-console.log(date3);
-
-// getters for Date object
-const date4 = new Date();
-console.log(date4.getFullYear());
-console.log(date4.getMonth());
-console.log(date4.getDate());
-console.log(date4.getDay());
-console.log(date4.getHours());
-console.log(date4.getMinutes());
-console.log(date4.getSeconds());
-console.log(date4.getTime()); // Epoch time
-
-// Setters for Date Object
-const date5 = new Date();
-date5.setFullYear(2020);
-date5.setMonth(10); // 10 = november - zero indexed
-date5.setDate(27); 
-date5.setHours(17);
-date5.setMinutes(5);
-date5.setSeconds(32);
-
-console.log(date5);
+const myCounter = createCounter();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+console.log(myCounter.getCount());
