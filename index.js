5c16442e0fea93684fbd5698fbaf44aeb5a8303d
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-21 07:01:33 +0530

    Super Keyword

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-// Inheritance      Allows a new class to inherit properties & methods
-//                  from its parent class.
-//                  Allows easy code reusability
+// Super    Keyword used to access properties of parent class
+//          We can call the constructor of the parent class 
+//          this = this object
+//          super = the parent
+
 
 class Animal {
-    isAlive = true;
-    eat(){
-        console.log(`${this.name} is eating!`);
-    }
-    sleep(){
-        console.log(`${this.name} is sleeping!`);
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
 }
 
 class rabbit extends Animal {
-    name = "rabbit";
+    constructor(name, age, runSpeed) {
+        super(name, age);
+        this.runSpeed = runSpeed;
+    }
 }
 class hawk extends Animal {
-    name = "hawk";
+    constructor(name, age, flySpeed) {
+        super(name, age);
+        this.flySpeed = flySpeed;
+    }
 }
 
-const rabbit1 = new rabbit();
-const hawk1 = new hawk();
+
+const rabbit1 = new rabbit("Bugs", 5, 25);
+const hawk1 = new hawk("hawksnest", 4, 43);
 
 console.log(rabbit1.name);
-rabbit1.eat();
+console.log(rabbit1.age);
+console.log(rabbit1.runSpeed);
