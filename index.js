0f364f9cd56394ad02fe0addd13d605c4c800f9e
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-22 11:22:45 +0530

    Stopwatch

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-// Digital clock
-let display = document.getElementById("dis2");
+let display = document.getElementById("timer");
+display.textContent = '00:00:00:00';
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
 
-function updateTime() {
-    const clk = new Date();
-    let hour = clk.getHours();
-    hour = hour > 12 ? (hour -= 12) : hour;
-    let meridian = hour > 12 ? "AM" : "PM";
-    //hour = hour % 12 || 12;
-    hour = hour.toString().padStart(2, 0);
-    let mins = clk.getMinutes().toString().padStart(2, 0);
-    let secs = clk.getSeconds().toString().padStart(2, 0);
-
-    let finalDisplay = hour + ":" + mins + ":" + secs + " " + meridian;
-    display.textContent = finalDisplay;
-    setTimeout(updateTime, 1000);
+function start() {
+    if (!isRunning) {
+        // display.textContent = 'hello';
+        startTime = Date.now() - elapsedTime; // To continue a stopped timer
+        isRunning = true;
+        timer = setInterval(update, 10);
+        // setInterval :    Repeatedly calls a function or executes a
+        //                  code snippet, with a fixed time delay between
+        //                  each call.
+    }
 }
-updateTime();
+function stop() {
+    if (isRunning) {
+        clearInterval(timer);   // tops the timer var started by setInterval;
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+}
+function reset() {
+    // this stops the case when reset is pressed while running
+    isRunning ? stop() : stop();
+    timer = null;
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = '00:00:00:00';
+}
+
+function update() {
+    // Current time = the global time rn
+    // Elapsed time = the time that has passed till start time (displayed time)
+    // Start Time   = the exact time when the timer was started
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+    console.log(currentTime);
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let mins = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let secs = Math.floor(elapsedTime / (1000) % 60);
+    let msec = Math.floor(elapsedTime % (1000) / 10);
+
+    hours = String(hours).padStart(2, '0');
+    mins = String(mins).padStart(2, '0');
+    secs = String(secs).padStart(2, '0');
+    msec = String(msec).padStart(2, '0');
+
+    let displayedTime = `${hours}:${mins}:${secs}:${msec}`;
+    display.textContent = displayedTime;
+}
