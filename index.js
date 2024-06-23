fc8605505eada9b0dfcb0c3fe140af6263d1755e
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-23 19:52:41 +0530

    mouse eventListeners()

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
-// DOM Manipulation
-// Overlooked chapters : 60 & 61
-// Continuing from 62
+// eventListener      Listen to specific events to create interactive webpages
+//                  events: click, mouseover, mouseout.
+//                  .addEventListener(event, callback)
 
-// S1: Create the element
-const newH1 = document.createElement("h1");
-
-// S2: ADD Attributes & Properties 
-newH1.textContent = `i love pizza`;
-newH1.id = `newH1`;
-newH1.style.color = `coral`;
-newH1.style.textAlign = `center`;
+const myBox = document.getElementById('myBox');
+const myButton = document.getElementById('button');
+// myBox.textContent=`hello`;
 
 
+// Method 1
+// function changeColor(event) {
+//     // target = the element that is being clicked 
+//     event.target.style.backgroundColor = 'tomato';
+//     event.target.textContent = 'Ouch!';
+// }
+// myBox.addEventListener('click', changeColor)
 
-// S3: Append elements to DOM
-// document.body.append(newH1);  // Adds as the last child in the body
-// document.body.prepend(newH1);  // Adds as the first child in the body
-// document.getElementById('box1').append(newH1);      // To append it inside a box
+// Method 2
+myBox.addEventListener('click', event => {
+    event.target.style.backgroundColor = 'tomato';
+    event.target.textContent = 'Ouch!';
+});
 
-//! NOT Working
-// const box2 = document.getElementById("box2");
-// // document.body.insertBefore(newElement,currentElement);
-// box2.style.color = `green`;
-// document.body.insertBefore(newH1, box2)     // Not working
+myBox.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = 'yellow';
+    event.target.textContent = 'Ayy!';
+});
 
-//! NOT Working again
-// // by using class + querySelectorAll
-// const boxes =document.querySelectorAll(".box");
-// document.body.insertBefore(newH1,boxes[2])
+myBox.addEventListener('mouseout', event => {
+    event.target.style.backgroundColor = 'lightgreen';
+    event.target.textContent = 'Click Me!';
+});
+
+myButton.addEventListener('click', event => {
+    event.target.style.backgroundColor = 'tomato';
+    event.target.textContent = 'Ouch!';
+});
+
+myButton.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = 'yellow';
+    event.target.textContent = 'Ayy!';
+});
+
+myButton.addEventListener('mouseout', event => {
+    event.target.style.backgroundColor = 'white';
+    event.target.textContent = 'Click Me!';
+});
+
