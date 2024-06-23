d67bf7c642e970e95d1a58db08dffcf84417e5ee
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-23 19:30:44 +0530

    ERROR : DOM Manuplatation not working (62)

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-let display = document.getElementById("input");
-setTimeout(()=>{display.value = ``},2000);
+// DOM Manipulation
+// Overlooked chapters : 60 & 61
+// Continuing from 62
 
-function appendToDisplay(input) {
-    display.value += input;
-}
+// S1: Create the element
+const newH1 = document.createElement("h1");
 
-function clearDisplay() {
-    display.value = `Cleared!`;
-    setTimeout(() => { display.value = ``; }, 500)
-}
+// S2: ADD Attributes & Properties 
+newH1.textContent = `i love pizza`;
+newH1.id = `newH1`;
+newH1.style.color = `coral`;
+newH1.style.textAlign = `center`;
 
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = 'ERR0R~';
-    }
-}
+
+
+// S3: Append elements to DOM
+// document.body.append(newH1);  // Adds as the last child in the body
+// document.body.prepend(newH1);  // Adds as the first child in the body
+// document.getElementById('box1').append(newH1);      // To append it inside a box
+
+//! NOT Working
+// const box2 = document.getElementById("box2");
+// // document.body.insertBefore(newElement,currentElement);
+// box2.style.color = `green`;
+// document.body.insertBefore(newH1, box2)     // Not working
+
+//! NOT Working again
+// // by using class + querySelectorAll
+// const boxes =document.querySelectorAll(".box");
+// document.body.insertBefore(newH1,boxes[2])
