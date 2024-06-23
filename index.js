d6372499c5158a9d8d84a6f976ebc6370c5b5133
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-23 12:55:37 +0530

    Calculator~

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
-// Errors Handling     
-//                  Errors when uncaught disrupts the flow of the program
-//                  usually by exiting prematurely
+let display = document.getElementById("input");
+setTimeout(()=>{display.value = ``},2000);
 
-/*
-    try(){}     :   The code that might cause the problem 
-    catch(){}   :   Catch and handel any errors thrown by try()
-    finally(){} :   Always runs weather the error is caught or not
-                    usually used for clean up, closing connection
-
-    throw       :   To manually throw an error
-                    throw new Error(msg);
-*/
-
-
-// try {
-//     console.log(x);
-//     // here the error is that x is not defined
-// } catch (error) {
-//     console.log(error);
-//     console.error(error);
-// } finally {
-//     console.log('The end of file');
-// }
-
-
-try {
-    let dividend = Number(window.prompt("Enter a dividend"));
-    let divisor = Number(window.prompt("Enter a divisor"));
-
-    if (divisor === 0) {
-        throw new Error('The divisor should be > 0');
-    }
-
-    if (isNaN(dividend) || isNaN(divisor)) {
-        throw new Error('The dividend & divisor should be a number!');
-    }
-    const res = dividend / divisor;
-    console.log(res);
-} catch (error) {
-    console.log(error);
+function appendToDisplay(input) {
+    display.value += input;
 }
-finally {
-    console.log('End of file');
+
+function clearDisplay() {
+    display.value = `Cleared!`;
+    setTimeout(() => { display.value = ``; }, 500)
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'ERR0R~';
+    }
 }
