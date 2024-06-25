66cb6f5cc615c4a3099e194ed10dfc1676137e23
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-25 13:33:11 +0530

    Rock Paper Scissor

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
-/*  classList
+// Rock Paper Scissor
 
-    .add()      :   Adds classes
-    .remove()   :   Removes class
-    .toggle()   :   remove if present, add if not
-    .replace()  :   (old-class, new-class)
-    .contains() :   Returns boolean
-*/
+const choicesAvailable = ['Rock', 'Paper', 'Scissor'];
+let rockBtn = document.getElementById("rock");
+let paperBtn = document.getElementById("paper");
+let scissorBtn = document.getElementById("scissor");
 
-let btn = document.getElementById('btn');
-btn.classList.add("active");
+let playerChoice = document.getElementById("playerChoice");
+let computerChoice = document.getElementById("computerChoice");
 
-btn.addEventListener('mouseover', event => {
-    // btn.classList.add("hover");  // don't directly access, use event target
-    event.target.classList.add("hover");
-})
-btn.addEventListener('mouseout', event => {
-    event.target.classList.remove("hover");
-})
+let resultDisplay = document.getElementById("result");
+
+let playerScoreDisplay = document.getElementById("playerScore");
+let computerScoreDisplay = document.getElementById("computerScore");
+
+let playerScore = 0;
+let computerScore = 0;
+
+playerScoreDisplay.textContent = `Player Score: 0`
+computerScoreDisplay.textContent = `Player Score: 0`
+
+
+function playGame(choice) {
+    let randChoice = choicesAvailable[Math.floor(Math.random() * 3)];
+    let result = "";
+    if (choice === randChoice) {
+        result = "It's a Tie!"
+    } else {
+        switch (choice) {
+            case "Rock":
+                result = (randChoice === "Scissor") ? "You Win!" : "You Lose!";
+                break;
+            case "Paper":
+                result = (randChoice === "Rock") ? "You Win!" : "You Lose!";
+                break;
+            case "Scissor":
+                result = (randChoice === "Paper") ? "You Win!" : "You Lose!";
+                break;
+        }
+    }
+    playerChoice.textContent = `Player Choice : ${choice}`;
+    computerChoice.textContent = `Computer Choice : ${randChoice}`;
+
+    resultDisplay.textContent = result;
+    resultDisplay.classList.remove("win", "lose", "tie");
+    switch (result) {
+        case "You Win!":
+            resultDisplay.classList.add("win");
+            playerScore += 1;
+            playerScoreDisplay.textContent = `Player Score: ${playerScore}`
+            break;
+        case "You Lose!":
+            resultDisplay.classList.add("lose");
+            computerScore += 1;
+            computerScoreDisplay.textContent = `Player Score: ${computerScore}`
+            break;
+        case "It's a Tie!":
+            resultDisplay.classList.add("tie");
+            break;
+    }
