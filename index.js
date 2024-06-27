b08e0e94278d8dd629be6e3648652ca53e61c0df
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-06-27 11:50:51 +0530

    Fetching data from API - Pokemon Search

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-/* JSON (Javascript Object Notation)
-    Data-interchange format
-    JSON Files: {key:value} or [value1, value2, value3]
-
-    JSON.stringify()    = convert a JS object to a JSON String
-    JSON.parse()        = convert a JSON string to a JS object
+/* Fetching data from an api
+    fetch() : Function used for making HTTP request to fetch resources
+    (JSON Style Data, Images, Files)
+    Simplifies asynchronous data fetching in JS and used for
+    interacting with APIs to retrieve and send data asynchronously 
+    over the web  
+    
+    fetch(url, {options})
+    {options} - 
+        method: GET (DEFAULT)   To get a resource   
+        method: POST            To send data
+        method: PUT             To replace some data
+        method: DELETE          To delete some data
 */
-const names = `["Spongebob", "Patrick", "Squidward", "Plankton"]`;
-const person = `{ "name": "Spongebob", "age": 26, "isEmployed": true, "hobbies": ["Cooking", "Jellyfishing", "Karate"] }`;
-const people = `[
-        { "name": "Spongebob", "age": 26, "isEmployed": true },
-        { "name": "Patrick", "age": 32, "isEmployed": false },
-        { "name": "Squidward", "age": 41, "isEmployed": true }
-]`;
 
-// JSON.Stringify()
-// console.log(names);
-// console.log(JSON.stringify(names));
+// Using ASYNC & AWAIT
 
-// JSON.parse()
-// console.log(names);
-// console.log(JSON.parse(names));
+let inputBox = document.getElementById("inpBox");
+let pokeImg = document.getElementById("pokeImg");
+
+async function fetchData() {
+        console.log(`input data: ${inputBox.value}`);
+        let input = String(inputBox.value).trim().toLowerCase();
+        console.log(`Sanitized data: ${input}`);
+        const URL = `https://pokeapi.co/api/v2/pokemon/${input}`;
+        try {
+                const response = await fetch(URL);
+                if (!response.ok) { throw new Error("Not a Pokemon") }
+                const data = await response.json();     // Long process, so you need await
+                console.log(data.name);
+                const image = data.sprites.front_default;
+                pokeImg.src = image;
+                pokeImg.alt = `input-${data.name}`;
+                pokeImg.style = "display: block";
+        } catch (err) {
+                console.error(err);
+        }
+}
 
 
-fetch("./JSON/person.json")
-        // .json converts the fetched data into a json object 
-        // & returns a promise i.e. the value of the json file
-        .then(response => response.json())
-        .then(value => console.log(value))
-        .catch(error => console.error(error));
+
+
+// Using JS promises chaining
+// const URL = `https://pokeapi.co/api/v2/pokemon/pikachu`;
+// fetch(URL)
+//         .then(response => {
+//                 if (!response.ok) throw new Error("Not a pokemon!");
+//                 return response.json();
+//         })
+//         .then(data => {
+//                 console.log(data.name);
+//                 console.log(data.weight);
+//         })
+//         .catch(error => console.error(error))
